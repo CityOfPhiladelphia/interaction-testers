2231e3f0bea5b7da497165029e8dbe60a25857dd
timwis <tim@timwis.com>
2015-11-26 13:23:02 -0500

    remove capture log

--- capture.js
+++ capture.js
@@ -1,6 +1,5 @@
 module.exports = function(interval) {
 	if(system.env.CAPTURE) {  // only capture if listening
-		console.log('capturing')
 		setInterval(function() {
 			casper.evaluate(function(img) {
 				__utils__.sendAJAX('http://localhost:8002', 'POST', {img: img}, false)
