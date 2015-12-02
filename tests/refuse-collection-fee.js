edebd20bd3789632cda9babc3a42add4181e2c16
Andrew Kennel <akennel@gmail.com>
2015-12-02 15:01:09 +0000

    Added "environmental", "refuse collection", and "streets closure".

---
+++ tests/refuse-collection-fee.js
@@ -0,0 +1,26 @@
+var system = require('system')
+require('../capture')(300)
+
+var NOTICE_NUMBER = system.env.REFUSE_NOTICE
+var OPA_NUMBER = system.env.REFUSE_OPA
+var url = 'https://secure.phila.gov/PaymentCenter/AccountLookup/PaymentLookup.aspx?lookup=4ede6c22-6e9d-43f5-bc12-2ddd22ba0441'
+var selectors = {
+	noticeInput: '#ctl00_BodyContentHolder_Manager_CriterionParameter_1_radTxt_text',
+	opaInput: '#ctl00_BodyContentHolder_Manager_CriterionParameter_0_radTxt_text',
+	continueButton: '#ctl00_BodyContentHolder_btnLookUp',
+	payButton: '#ctl00_BodyContentHolder_btnContinue'
+}
+
+casper.test.begin('Streets Closures - Escrow', function suite(test) {
+	casper.start(url, function() {
+		test.assertTitle('Payment Center - City of Philadelphia')
+		this.sendKeys(selectors.noticeInput, NOTICE_NUMBER)
+		this.sendKeys(selectors.opaInput, OPA_NUMBER)
+		this.click(selectors.continueButton)
+		this.waitForSelector(selectors.payButton, function() {
+			test.assertEquals(this.getElementAttribute(selectors.payButton, 'value'), 'Pay Now', '"Pay Now" button exists')
+		})
+	})
+	
+	casper.run()
+})
---
+++ tests/streets-closure-escrow.js
@@ -0,0 +1,26 @@
+var system = require('system')
+require('../capture')(300)
+
+var COMPANY_ID = system.env.STREETS_CLOSURES_COMPANY
+var TAX_ID = system.env.STREETS_CLOSURES_TAX
+var url = 'https://secure.phila.gov/PaymentCenter/AccountLookup/PaymentLookup.aspx?lookup=44a34533-6786-4c28-81fa-5deeb99ae718'
+var selectors = {
+	companyInput: '#ctl00_BodyContentHolder_Manager_CriterionParameter_0_radTxt_text',
+	taxInput: '#ctl00_BodyContentHolder_Manager_CriterionParameter_1_radTxt_text',
+	continueButton: '#ctl00_BodyContentHolder_btnLookUp',
+	payButton: '#ctl00_BodyContentHolder_btnContinue'
+}
+
+casper.test.begin('Streets Closures - Escrow', function suite(test) {
+	casper.start(url, function() {
+		test.assertTitle('Payment Center - City of Philadelphia')
+		this.sendKeys(selectors.companyInput, COMPANY_ID)
+		this.sendKeys(selectors.taxInput, TAX_ID)
+		this.click(selectors.continueButton)
+		this.waitForSelector(selectors.payButton, function() {
+			test.assertEquals(this.getElementAttribute(selectors.payButton, 'value'), 'Pay Now', '"Pay Now" button exists')
+		})
+	})
+	
+	casper.run()
+})
